e3ec95f29fa0fefd40cde6466861b37942b3347f
Anders Beck <andersbeck92@gmail.com>
2023-04-02 14:03:48 +0200

    image hash

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import hashlib
+from PIL import Image
+import urllib.request
+import io
 
 app = Flask(__name__)
-port = 5000
+PORT = 5000
+
+FILE_FORMATS = ['jpg', 'jpeg', 'png']
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/get_image_hash', methods=['POST'])
+def get_image_hash():
+    image_url = request.json['image_url']
+    file_ext = image_url.split('.')[-1].lower()
+
+    if file_ext not in FILE_FORMATS:
+        return jsonify({'error': 'Invalid file format. Only JPG, JPEG and PNG files are allowed.'}), 400
+
+    image_data = urllib.request.urlopen(image_url).read()
+    img = Image.open(io.BytesIO(image_data))
+    image_hash = hashlib.sha256(img.tobytes()).hexdigest()
+    return {'image_hash': image_hash}
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
